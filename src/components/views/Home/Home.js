8be14e96a1627da4f1cf419f953d07d305c213ea
Bartosz Glinka <bartosz.glinka@hotmail.com--global>
2020-07-28 23:25:32 +0200

    install material-ui in supage views

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -1,13 +1,72 @@
 import React from 'react';
 import styles from './Home.module.scss';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
 
-const Home = () => {
-  return(
-    <div className={styles.component}>
-      <h2>Home view</h2>
-    </div>
-  );
+const demoContent = [
+  {id: '1', status: 'Login',},
+  {id: '2', status: 'Tables',},
+  {id: '3', status: 'Waiter',},
+  {id: '4', status: 'Kitchen'},
+];
+
+const renderActions = status => {
+  const preventDefault = (event) => event.preventDefault();
+  switch (status) {
+    case 'Login':
+      return (
+        <Button variant="contained" color="primary"><Link color="inherit" href='/panel/panel/login'>Login</Link></Button>
+      );
+    case 'Tables':
+      return (
+        <Button variant="contained" color="primary"><Link color="inherit" href='/panel/panel/tables'>Tables</Link></Button>
+      );
+    case 'Waiter':
+      return (
+        <Button variant="contained" color="primary"><Link color="inherit" href='/panel/panel/waiter'>Waiter</Link></Button>
+      );
+    case 'Kitchen':
+      return (
+        <Button variant="contained" color="primary"><Link color="inherit" href='/panel/panel/kitchen'>Kitchen</Link></Button>
+      );
+    default:
+      return null;
+  }
 };
 
+const Home = () => (
+  <Paper className={styles.component}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Number</TableCell>
+          <TableCell>Subpage Name</TableCell>
+          <TableCell>Link</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {demoContent.map(row => (
+          <TableRow key={row.id}>
+            <TableCell component="th" scope="row">
+              {row.id}
+            </TableCell>
+            <TableCell>
+              {row.status}
+            </TableCell>
+            <TableCell>
+              {renderActions(row.status)}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </Paper>
+);
 
 export default Home;
--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-const Login = () => {
-  return(
-    <div className={styles.component}>
-      <h2>Login view</h2>
-    </div>
-  );
-};
 
+
+const Login = () => (
+  <Paper className={styles.component}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>User Name</TableCell>
+          <TableCell>Password</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+          <TableRow>
+            <TableCell component="th" scope="row">
+              <TextField required id="standard-required" label="Required" defaultValue="Login" />
+            </TableCell>
+            <TableCell>
+              <TextField id="standard-password-input" label="Password" type="password" autoComplete="current-password" />
+            </TableCell>
+            <TableCell>
+              <Button variant="contained" color="primary">Zaloguj się</Button>
+         </TableCell>
+          </TableRow>
+      </TableBody>
+    </Table>
+  </Paper>
+);
 
 export default Login;
